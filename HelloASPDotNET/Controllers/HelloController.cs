2d9a56488e5025569384c21b805504e7ebe95490
Chance Backus <cbackusjr@outlook.com>
2021-07-20 11:49:52 -0700

    ch13 branch created

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -10,66 +10,26 @@ namespace HelloASPDotNET.Controllers
     public class HelloController : Controller
     {
 
-        //GET: /<controller>/
-        [HttpGet]
-        
-        /*public IActionResult Index()
-        {
-            string html = "<form method='post' action='/helloworld/'>" + 
-                "<input type='text' name='name' />" + 
-                "<input type='submit' value='Greet Me!' />" +
-                "</form>";
-            return Content(html, "text/html");
-        }*/
-
-
+{
+        // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" +
+            string html = "<form method='post' action='/hello'>" +
                 "<input type='text' name='name' />" +
-                "<select name='language'><option value='1' selected>English</option><option value='2'>French</option>" +
-                "<option value='3'>Spanish</option><option value='4'>Haitian-Dialect</option><option value='5'>Polish</option></select>" +
                 "<input type='submit' value='Greet Me!' />" +
-                "</form>";                
+                "</form>";
 
             return Content(html, "text/html");
         }
 
         [HttpPost]
-        public IActionResult Name(int language, string name = "World")
-        {
-            if(language == 1)
-            {
-                return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
-            }
-            else if (language == 2)
-            {
-                return Content("<h1>Bonjour, " + name + "!</h1>", "text/html");
-            }
-            else if (language == 3)
-            {
-                return Content("<h1>¡Hola, " + name + "!</h1>", "text/html");
-            }
-            else if (language == 4)
-            {
-                return Content("<h1>Sak-Pase, " + name + "!</h1>", "text/html");
-            }
-            else
-            {
-                return Content("<h1>Witamy, " + name + "!</h1>", "text/html");
-            }
-        }
-
-
-        //GET: 
-       /* [HttpGet("/welcome/{name?}"]
-        [HttpPost]
-        
+        [Route("/hello")]
         public IActionResult Welcome(string name = "World")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>" , "text/html");
-        }*/
-        
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
     }
+
+}
 }
